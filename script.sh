99cee8eb68b4e1faf2833b178cb8356b4442ea1d
Alexandru Bostan <alex.bostan007@gmail.com>
2024-06-20 12:25:18 +0300

    changes to script.sh

--- script.sh
+++ script.sh
@@ -3,25 +3,32 @@
 set -eo pipefail
 
 EFI='/dev/nvme0n1p1'
-ROOT='/dev/nvme0n1p2'
+ROOT='/dev/nvme0n1p4'
 DRIVE='/dev/nvme0n1'
 EFIPART=1
 
-mkfs.ext4 $ROOT
-mount $ROOT /mnt
-mount --mkdir $EFI /mnt/efi
+normal_fs () {
+    mkfs.ext4 $ROOT
+    mount $ROOT /mnt
+    mount --mkdir $EFI /mnt/efi
+}
 
-#cryptsetup luksFormat $ROOT
-#cryptsetup --allow-discards --perf-no_read_workqueue --perf-no_write_workqueue --persistent open $ROOT root
-#mkfs.ext4 /dev/mapper/root
-#mount /dev/mapper/root /mnt
-#mount --mkdir $EFI /mnt/efi
+luks_fs () {
+    cryptsetup luksFormat $ROOT
+    cryptsetup --allow-discards --perf-no_read_workqueue --perf-no_write_workqueue --persistent open $ROOT root
+    mkfs.ext4 /dev/mapper/root
+    mount /dev/mapper/root /mnt
+    mount --mkdir $EFI /mnt/efi
+}
+
+normal_fs
+#luks_fs
 
 pacstrap -K /mnt base linux linux-firmware vim sudo \
 efibootmgr \
 networkmanager \
 terminus-font \
-intel-ucode
+amd-ucode
 
 echo '%wheel      ALL=(ALL:ALL) NOPASSWD: ALL' | tee -a /mnt/etc/sudoers > /dev/null
 
@@ -54,23 +61,30 @@ default_options="--splash=/usr/share/systemd/bootctl/splash-arch.bmp"
 EOF
 
 tee /mnt/etc/mkinitcpio.conf > /dev/null << EOF
-MODULES=(i915)
-#MODULES=(amdgpu)
+#MODULES=(i915)
+MODULES=(amdgpu)
+
 BINARIES=()
+
 FILES=()
+
 HOOKS=(systemd autodetect microcode modconf keyboard sd-vconsole block filesystems fsck)
 #HOOKS=(systemd autodetect microcode modconf keyboard sd-vconsole block sd-encrypt filesystems fsck)
 EOF
 
-mkdir -p /mnt/efi/EFI/Linux
-rm -rf /mnt/efi/EFI/Linux/*
-arch-chroot /mnt mkinitcpio -p linux
+make_efi () {
+    mkdir -p /mnt/efi/EFI/Linux
+    rm -rf /mnt/efi/EFI/Linux/*
+    arch-chroot /mnt mkinitcpio -p linux
 
-systemctl enable NetworkManager --root=/mnt
+    efibootmgr --create \
+    --disk $DRIVE --part $EFIPART \
+    --label "Arch Linux" \
+    --loader 'EFI/Linux/arch-linux.efi' \
+    --unicode
+}
+
+#make_efi
+
+systemctl enable NetworkManager.service --root=/mnt
 systemctl enable fstrim.timer --root=/mnt
-
-efibootmgr --create \
---disk $DRIVE --part $EFIPART \
---label "Arch Linux" \
---loader 'EFI/Linux/arch-linux.efi' \
---unicode
